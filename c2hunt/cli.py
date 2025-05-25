79d912bb982d8bc768fad0281e707f9ed4d5694a
krnick <sungboss2004@gmail.com>
2025-05-25 09:12:34 +0800

    first commit

---
+++ c2hunt/cli.py
@@ -0,0 +1,39 @@
+import click
+
+from c2hunt.main import analyze_target, get_smali
+
+
+@click.command()
+@click.option(
+    "-f", "--file",
+    required=True,
+    type=click.Path(exists=True),
+    help="Target APK or DEX file."
+)
+@click.option(
+    "-o", "--opcode",
+    default="custom-opcode/switch-equals.json",
+    show_default=True,
+    type=click.Path(exists=True),
+    help="Path to the custom opcode JSON file.",
+)
+@click.option(
+    "-p", "--print-smali",
+    is_flag=True,
+    help="Print all smali methods from the target APK/DEX instead of scanning."
+)
+def cli(file: str, opcode: str, print_smali: bool) -> None:
+    """
+    C2Hunt - Analyze Android APK/DEX files for C2 commands.
+    """
+    if print_smali:
+        click.secho(f"[INFO] Extracting smali from: {file}", fg="cyan")
+        get_smali(file)
+    else:
+        click.secho(f"[INFO] Analyzing: {file}", fg="cyan")
+        click.secho(f"[INFO] Using Opcode & APIs definition: {opcode}", fg="cyan")
+        analyze_target(file, opcode)
+
+
+if __name__ == "__main__":
+    cli()
