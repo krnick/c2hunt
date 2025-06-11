e93b035bffe0ea3c927be5436bf87c3b9fdc4ff2
krnick <sungboss2004@gmail.com>
2025-06-11 14:36:51 +0800

    version 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="c2hunt",
-    version="0.0.4",
+    version="0.0.5",
     description="Hunting Potential C2 Commands in Android Malware via Smali String Comparison and Control Flow Analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
